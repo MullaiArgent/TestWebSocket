fc1ae9246fc327a59c8974c358859006fec46de2
Mullai Rajan <86584609+MullaiArgent@users.noreply.github.com>
2022-02-09 23:37:14 +0530

    Initial

---
+++ target/TestWebSocket-1.0-SNAPSHOT/secure/login.jsp
@@ -0,0 +1,141 @@
+<%@ page import="java.sql.SQLException" %>
+<%@ page import="java.sql.ResultSet" %>
+<%@ page import="datamanagement.JDBC" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css-login">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet">
+    <meta charset="ISO-8859-1" content="">
+    <title>Login</title>
+    <style>
+        Body {
+            font-family: Calibri, Helvetica, sans-serif;
+            background-color: white;
+        }
+        button {
+            background-color: #05728f;
+            width: 100%;
+            color: white;
+            padding: 15px;
+            margin: 10px 0;
+            border: none;
+            cursor: pointer;
+        }
+        button a{
+            color: white;
+        }
+        form {
+            border: 3px solid #f1f1f1;
+        }
+        input[type=text], input[type=password] {
+            width: 100%;
+            margin: 8px 0;
+            padding: 12px 20px;
+            display: inline-block;
+            border: 2px solid  #05728f;
+            box-sizing: border-box;
+        }
+        button:hover {
+            opacity: 0.7;
+        }
+        .cancelbtn {
+            width: auto;
+            padding: 10px 18px;
+            margin: 10px 5px;
+        }
+
+
+        .container {
+            padding: 50px;
+            background-color: white;
+            position:relative;
+            width:fit-content;
+            max-height: fit-content;
+
+        }
+        p{
+            text-align: center;
+        }
+
+    </style>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<%
+    response.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
+%>
+<div style="text-align: center;"> <h1> Login </h1> </div>
+<div class="container">
+        <%
+            final JaasSecurity.GoogleOAuth helper = new JaasSecurity.GoogleOAuth();
+            if (request.getParameter("code") == null || request.getParameter("state") == null) {
+
+                out.println("<form action=\"j_security_check\" method=\"POST\">");
+                out.println("<label>Username : </label>");
+                out.println("<input type=\"text\" placeholder=\"Enter Username\" name= \"j_username\" required>");
+                out.println("<label>Password : </label>");
+                out.println("<input type=\"password\" placeholder=\"Enter Password\" name= \"j_password\" required>");
+                out.println("<button type=\"submit\" value = \"check\">Login</button>");
+                out.println("</form>");
+                out.println("<form action=\"adduser\">" +
+                        "    <button type=\"submit\">" +
+                        "        Sign-Up" +
+                        "    </button>\n" +
+                        "</form>");
+                out.println("<p>----------- or -----------</p>");
+                out.println("<button><a href='" + helper.buildLoginUrl() + "'> Login using Google </a></button>");
+
+                session.setAttribute("state", helper.getStateToken());
+            } else if (request.getParameter("code") != null && request.getParameter("state") != null
+                    && request.getParameter("state").equals(session.getAttribute("state"))) {
+
+                JDBC db = new JDBC();
+
+                String userinfo = helper.getUserInfoJson(request.getParameter("code"));
+                String picture = JaasSecurity.GoogleOAuth.getValues(userinfo,"picture");
+                String givenName =  JaasSecurity.GoogleOAuth.getValues(userinfo,"given_name");
+                String name = JaasSecurity.GoogleOAuth.getValues(userinfo,"name");
+                String email = JaasSecurity.GoogleOAuth.getValues(userinfo,"email");
+                boolean createNew = true;
+                try {
+                    ResultSet rs = db.dql("SELECT \"ID\" FROM public.\"USERS\"");
+                    while (rs.next()){
+                        if(rs.getString(1).equals(email)){
+                            createNew = false;
+                        }
+                    }
+
+                } catch (SQLException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                if (createNew) {
+                    out.println("<form action=\"registerGoogleUser\" method=\"POST\">");
+                    out.println("<input value=\"" + picture + "\" name=\"picture\" hidden>");
+                    out.println("<input value=\"" + name + "\" name=\"name\" hidden>");
+                    out.println("<input value=\"" + email + "\" name=\"email\" hidden>");
+                    out.println("<img src=" + picture + " alt=" + givenName + ">");
+                    out.println("<h4>Hello " + givenName + " !! </h4><br>");
+                    out.println("<input type=\"text\" value=\"givenName\" name=\"AppSpecUser\" hidden><br>");
+                    out.println("<button type=\"submit\" value = \"check\">Sign in as "+ givenName +" </button>");
+                    out.println("</form>");
+                }else{
+                    session.removeAttribute("state");
+                    out.println("<img src=" + picture + " alt=" + givenName + ">");
+                    out.println("<form action=\"j_security_check\" method=\"POST\">");
+                    out.println("<input type=\"text\" value=\""+email+"\" name=\"j_username\" hidden>");
+                    out.println("<input type=\"password\" value=\""+ helper.getAccessToken() +"\" name= \"j_password\" hidden>");
+                    out.println("<button type=\"submit\" value =\"check\">Login in as "+ givenName +" </button>");
+                    out.println("</form>");
+                }
+            }
+        %>
+</div>
+</body>
+</html>
---
+++ target/TestWebSocket-1.0-SNAPSHOT/websocket.js
@@ -0,0 +1,269 @@
+window.onload = init;
+const socket = new WebSocket("ws://localhost:8080/TestWebSocket_war/actions");
+socket.onmessage = onMessage;
+let globalFriendId = "";
+function init(){
+
+}
+function printRecentChats(recentChat){
+
+    const recentChatsUl = document.getElementById("recentChatsUl");
+    const clearfix = document.createElement("li");
+    clearfix.setAttribute("class","clearfix");
+    clearfix.onclick = function () {
+        viewChat(recentChat.name);
+    }
+    recentChatsUl.appendChild(clearfix);
+
+    const avatar = document.createElement("img");
+    avatar.setAttribute("src","" + recentChat.profile);
+    avatar.setAttribute("alt",recentChat.name)
+    clearfix.appendChild(avatar);
+
+    const about = document.createElement("div");
+    about.setAttribute("class","about");
+    clearfix.appendChild(about);
+
+    const name = document.createElement("div");
+    name.setAttribute("class","name");
+    name.innerHTML = recentChat.name;
+    about.appendChild(name);
+
+}
+function printChat(chat){
+    const chatHistory = document.getElementById("chatList")
+
+    const chatIl = document.createElement("li");
+    chatIl.setAttribute("class", "clearfix");
+    chatHistory.appendChild(chatIl)
+
+    if (chat.from === "you"){
+        const messageData = document.createElement("div");
+        messageData.setAttribute("class","message-data text-right");
+        chatIl.appendChild(messageData);
+
+        const messageTime = document.createElement("span");
+        messageTime.setAttribute("class","message-data-time");
+        messageTime.innerHTML = chat.time;
+        messageData.appendChild(messageTime);
+
+        const chatData = document.createElement("div");
+        chatData.setAttribute("class","message other-message float-right")
+        chatData.innerHTML = chat.data;
+        chatIl.appendChild(chatData);
+
+    }else{
+        const messageData = document.createElement("div");
+        messageData.setAttribute("class","message-data");
+        chatIl.appendChild(messageData);
+
+        const messageTime = document.createElement("span");
+        messageTime.setAttribute("class","message-data-time");
+        messageTime.innerHTML = chat.time;
+        messageData.appendChild(messageTime);
+
+        const chatData = document.createElement("div");
+        chatData.setAttribute("class","message my-message")
+        chatData.innerHTML = chat.data;
+        chatIl.appendChild(chatData);
+    }
+}
+function removeChat(){
+    const chatList = document.getElementById("chatList")
+    chatList.innerHTML = "";
+}
+function onMessage(event){
+    const message = JSON.parse(event.data);
+    if (message.action === "addRecentChatModel"){
+        printRecentChats(message);
+    }
+    if (message.action === "viewChat"){
+        printChat(message);
+    }
+    if (message.action === "notificationCount"){
+        const Count = document.getElementById("badge");
+        Count.innerHTML = message.notificationCount;
+    }
+    if (message.action === "addFriendWindow"){
+        const windowType = document.getElementById("forProfile");
+
+        const profile = document.createElement("img");
+        profile.setAttribute("src",message.profile);
+        profile.setAttribute("height","100");
+        profile.setAttribute("width","100");
+
+        windowType.appendChild(profile);
+
+        const popWindowType = document.getElementById("popWindowType");
+        popWindowType.innerHTML = "Send Friend Request";
+
+        const popWindowFriendName = document.getElementById("popWindowFriendName");
+        popWindowFriendName.innerHTML = message.friendId;
+
+        const popWindowButton = document.getElementById("popWindowButton");
+        popWindowButton.innerHTML = "Add Friend"
+        popWindowButton.onclick = function () {
+            sendFriendRequest(message.friendId);
+            overlayOff();
+            console.log("add friend request to "+ message.friendId)
+        }
+        overlayOn();
+    }
+    if (message.action === "invitationWindow"){
+        const windowType = document.getElementById("forProfile");
+
+        const profile = document.createElement("img");
+        profile.setAttribute("src", "https://tinyurl.com/2p8hr77v");
+        profile.setAttribute("height","100");
+        profile.setAttribute("width","100");
+
+        windowType.appendChild(profile);
+
+        const popWindowType = document.getElementById("popWindowType");
+        popWindowType.innerHTML = "Send Invitation";
+
+        const popWindowFriendName = document.getElementById("popWindowFriendName");
+        popWindowFriendName.innerHTML = message.friendId;
+
+        const popWindowButton = document.getElementById("popWindowButton");
+        popWindowButton.innerHTML = "Invite"
+        popWindowButton.onclick = function () {
+            sendInvitation(message.friendId);
+            console.log("Invitation to "+ message.friendId);
+            overlayOff();
+        }
+        overlayOn();
+    }
+    if (message.action === "chatWindow"){
+        const windowType = document.getElementById("forProfile");
+
+        const profile = document.createElement("img");
+        profile.setAttribute("src",message.profile);
+        profile.setAttribute("height","100");
+        profile.setAttribute("width","100");
+
+        windowType.appendChild(profile);
+
+        const popWindowType = document.getElementById("popWindowType");
+        popWindowType.innerHTML = "Already a Friend";
+
+        const popWindowFriendName = document.getElementById("popWindowFriendName");
+        popWindowFriendName.innerHTML = message.friendId;
+
+        const popWindowButton = document.getElementById("popWindowButton");
+        popWindowButton.innerHTML = "Chat"
+        popWindowButton.onclick = function (){
+            viewChat(message.friendId);
+            overlayOff();
+        }
+        overlayOn();
+    }
+    if (message.action === "alreadyInvited"){
+        const windowType = document.getElementById("forProfile");
+
+        const profile = document.createElement("img");
+        profile.setAttribute("src", "https://tinyurl.com/yztecup8");
+        profile.setAttribute("height","100");
+        profile.setAttribute("width","100");
+
+        windowType.appendChild(profile);
+
+        const popWindowType = document.getElementById("popWindowType");
+        popWindowType.innerHTML = "Already Invited";
+
+        const popWindowFriendName = document.getElementById("popWindowFriendName");
+        popWindowFriendName.innerHTML = message.friendId;
+
+        const popWindowButton = document.getElementById("popWindowButton");
+        popWindowButton.innerHTML = "Close"
+        popWindowButton.onclick = function () {
+            overlayOff();
+        }
+        overlayOn();
+    }
+    if (message.action === "newNotification"){
+        const badge = document.getElementById("badge");
+        badge.innerHTML = parseInt(badge.innerHTML) + 1;
+    }
+
+    if (message.action === ""){
+
+    }
+}
+function viewChat(friendId){
+    globalFriendId = friendId;
+    const ChatAction = {
+        action: "viewChat",
+        friendId: friendId
+    };
+    const ul = document.getElementById("chatList");
+    if (document.contains(ul)){
+        removeChat();
+    }
+    socket.send(JSON.stringify(ChatAction));
+}
+function sendMessage(){
+    const message = document.getElementById("messageToSend").value;
+    document.getElementById("messageToSend").value = "";
+    const date = new Date();
+    if (globalFriendId !== ""){
+        const ChatAction = {
+            action: "sendMessage",
+            friendId: globalFriendId,
+            message: message + "",
+            time : "" + date.getHours() + ":" + date.getMinutes()
+        };
+        socket.send(JSON.stringify(ChatAction));
+    }
+}
+function sendFriendRequest(friendId){
+    const ChatAction = {
+        action: "friendRequest",
+        friendId: friendId
+    };
+    socket.send(JSON.stringify(ChatAction));
+}
+function sendInvitation(friendId){
+    const ChatAction = {
+        action: "invitation",
+        friendId: friendId
+    };
+    socket.send(JSON.stringify(ChatAction));
+}
+function viewNotification(){
+    var ChatAction = {
+        action : "viewNotification"
+    };
+    socket.send(JSON.stringify(ChatAction));
+}
+function confirmFriendRequest(){
+    var ChatAction = {
+        action : "confirmFriendRequest",
+        friendId : ""
+    };
+    socket.send(JSON.stringify(ChatAction));
+}
+function declineFriendRequest(){
+    const ChatAction = {
+        action: "declineFriendRequest",
+        friendId: ""
+    };
+    socket.send(JSON.stringify(ChatAction));
+}
+function overlayOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+function overlayOff() {
+    document.getElementById("overlay").style.display = "none";
+    const popWindowProfile = document.getElementById("forProfile");
+    popWindowProfile.innerHTML = "";
+}
+function addFriend(){
+    const friendId = document.getElementById("addFriend").value;
+    document.getElementById("addFriend").value = "";
+    const ChatAction ={
+        action : "addFriend",
+        friendId : friendId
+    };
+    socket.send(JSON.stringify(ChatAction));
+}
